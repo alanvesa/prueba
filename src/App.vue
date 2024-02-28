2b46708a6b0aa3020cda85bd7e59a30431359ec4
alanvesa <147011304+alanvesa@users.noreply.github.com>
2024-02-27 20:49:33 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -34,6 +34,7 @@
           <th>Actividad</th>
           <th>Prioridad</th>
           <th>Fecha</th>
+          <th>Eliminar</th>
         </tr>
       </thead>
       <tbody>
@@ -41,6 +42,9 @@
           <td>{{ item.activity }}</td>
           <td>{{ item.priority }}</td>
           <td>{{ item.date }}</td>
+          <td>
+            <button @click="eliminar(i)" class="eliminar">❌</button>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -86,7 +90,6 @@ const agregar = () => {
 
 };
 
-
 function limpiar() {
     activity.value = "";
     check.value = "";
@@ -101,6 +104,9 @@ function ordenar() {
     return a.priority === 'alta' ? -1 : 1;
   });
 }
+function eliminar(i) {
+  registros.value.splice(i, 1)
+}
 
 </script>
 
@@ -110,6 +116,14 @@ body {
   display: grid;
   place-items: center;
 }
+.eliminar{
+  width: 40%;
+  height: auto;
+  margin-left: 25%;
+  border-radius: 5px;
+  font-size: 120%;
+padding: 1%;
+  }
 
 
 input{
@@ -169,315 +183,3 @@ font-size: 30px;
   text-align: center;
 }
 </style>
-
-<!-- <template>
-  <div id="padre">
-    <h1>{{ parseInt(num) / parseInt(num2==0?1:num2) }}</h1>
-    <input type="text" placeholder="Telefono..." v-model.trim="num">
-    <input type="text" placeholder="Telefono..." v-model="num2"> --> -->
-
-   <!--  <h1>{{ contador }}</h1>
-    <button @click="contar()">Cambiar</button>
-    <h2 v-if="contador<21">Hola soy el msj q van a desaparecer</h2>
-    <h2 v-else>hola yo voy aparecer</h2>
-    <h2 > {{contador<21?"Hola soy el msj q van a desaparecer":"hola yo voy aparecer"}}</h2> -->
-
-    <!-- <h3 :style="alerta==='Formulario enviado correctamente'?'color:green':''">{{ alerta }}</h3>
-    <h1>Formulario</h1>
-    <form>
-      <label for="nombre">Nombre:</label>
-      <input type="text" id="nombre" name="nombre" v-model="nombre">
-      <label for="apellidos">Apellidos:</label>
-      <input type="text" id="apellidos" name="apellidos" v-model="apellidos">
-      <label for="tipo_documento">Tipo documento:</label>
-      <select id="tipo_documento" name="tipo_documento" v-model="tipo_documento">
-        <option value="cedula">Cédula</option>
-        <option value="tarjeta_identidad">Tarjeta de identidad</option>
-        <option value="pasaporte">Pasaporte</option>
-      </select>
-      <label for="numero_documento">Número de documento:</label>
-      <input type="text" id="numero_documento" name="numero_documento" v-model="numero_documento">
-      <label for="genero">Genero:</label>
-      <select id="genero" name="genero" v-model="genero">
-        <option value="masculino">Masculino</option>
-        <option value="femenino">Femenino</option>
-        <option value="otro">Otro</option>
-      </select>
-      <label for="correo">Correo:</label>
-      <input type="email" id="correo" name="correo" v-model="correo">
-      <label for="telefono">Teléfono:</label>
-      <input type="text" id="telefono" name="telefono" v-model="telefono">
-      <label for="fecha_nacimiento">Fecha de nacimiento:</label>
-      <input type="date" id="fecha_nacimiento" name="fecha_nacimiento" v-model="fecha_nacimiento">
-      <label for="telefono">Edad:</label>
-      <input type="number" id="telefono" name="telefono" v-model="edad1">
-    </form>
-    <button @click="validar()">enviar</button>
-
-    <table>
-      <thead>
-        <tr>
-          <th>Nombre</th>
-          <th>Apellido</th>
-          <th>Tipo de documento</th>
-          <th>Numero de documento</th>
-          <th>Telefono</th>
-          <th>Correo</th>
-          <th>Genero</th>
-          <th>Fecha de nacimiento</th>
-          <th>edad</th>
-          <th>Opciones</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(item, i) in registros" :key="i" :style="item.edad>17?'color:pink':'color:purple'">
-          <td>{{ item.nombre }}</td>
-          <td>{{ item.apellidos }}</td>
-          <td>{{ item.tipo_documento }}</td>
-          <td>{{ item.numero_documento }}</td>
-          <td>{{ item.telefono }}</td>
-          <td>{{ item.correo }}</td>
-          <td>{{ item.genero }}</td>
-          <td>{{ item.fecha_nacimiento }}</td>
-          <td>{{ item.edad }}</td>
-          <td>
-            <button @click="eliminar(i)">❌</button>
-            <button @click="editar(item, i)">📝</button>
-          </td>
-        </tr>
-
-      </tbody>
-
-    </table> 
-
-    <!-- <img :src="image" alt="" :style="{borderRadius:num}"> -->
-    <!-- <h1 :style="{borderRadius:12}">hola a todos</h1> -->
-    <!-- <h1 :style="num>18?'color:salmon':'color:green'">mi edad es {{ num }}</h1> -->
-
-  <!-- </div> -->
-<!-- </template>
-
-<script setup>
-import {ref} from "vue"
-let num = ref(19)
-let color = ref("yellow")
-let image = ref("https://adrianalonso.es/wp-content/uploads/2018/01/vue.jpg")
-
-let bd=true
-
-const registros = ref([{
-  nombre: "jose",
-  apellidos: "HOla",
-  tipo_documento: "CC",
-  numero_documento:"123123123",
-  telefono: "123123213",
-  correo: "asdas",
-  genero: "m",
-  fecha_nacimiento: "12312",
-  edad: "12"
-},
-{
-  nombre: "jmiguel",
-  apellidos: "HOla",
-  tipo_documento: "CC",
-  numero_documento: "123123123",
-  telefono: "123123213",
-  correo: "asdas",
-  genero: "m",
-  fecha_nacimiento: "12312",
-  edad: "12"
-},
-{
-  nombre: "cvcijiase",
-  apellidos: "HOla",
-  tipo_documento: "CC",
-  numero_documento: "123123123",
-  telefono: "123123213",
-  correo: "asdas",
-  genero: "m",
-  fecha_nacimiento: "12312",
-  edad: "12"
-},
-{
-  nombre: "jadasdnsae",
-  apellidos: "HOla",
-  tipo_documento: "CC",
-  numero_documento: "123123123",
-  telefono: "123123213",
-  correo: "asdas",
-  genero: "m",
-  fecha_nacimiento: "12312",
-  edad: "12"
-}])
-
-const nombre = ref("")
-const apellidos = ref("")
-const tipo_documento = ref("")
-const numero_documento = ref("")
-const genero = ref("")
-const correo = ref("")
-const telefono = ref("")
-const fecha_nacimiento = ref("")
-const edad1 = ref(0)
-let alerta = ref("")
-let index = null
-
-function eliminar(i){
-  registros.value.splice(i,1)
-}
-
-function editar(item, i){
-  console.log(item);
-  console.log(i);
-  nombre.value = item.nombre
-  apellidos.value = item.apellidos
-bd = false
-index=i
-
-}
-
-function ocultarAlerta() {
-  setTimeout(() => {
-  alerta.value = "";
-}, 3500)
-}
-
-
-const validar = () => {
-  if (bd==true){
-    if (nombre.value === "") {
-    alerta.value = "El campo nombre no puede estar vacio" -->
-    <!-- ocultarAlerta()
-  }
-  else if (apellidos.value === "") {
-    alerta.value = "El campo apellidos no puede estar vacio"
-    ocultarAlerta()
-  }
-  else if (tipo_documento.value === "") {
-    alerta.value = "El campo tipo de documento no puede estar vacio"
-    ocultarAlerta()
-  }
-  else if (numero_documento.value === "") {
-    alerta.value = "El campo número de documento no puede estar vacio"
-    ocultarAlerta()
-  }
-  else if (isNaN(numero_documento.value)) {
-    alerta.value = "El campo número de documento solo puede contener números"
-    ocultarAlerta()
-  }
-  else if (genero.value === "") {
-    alerta.value = "El campo genero no puede estar vacio"
-    ocultarAlerta()
-  }
-  else if (correo.value === "") {
-    alerta.value = "El campo correo no puede estar vacio"
-    ocultarAlerta()
-  }
-  else if (!correo.value.includes("@")) {
-    alerta.value = "El campo correo debe ser un correo valido"
-    ocultarAlerta()
-  }
-  else if (telefono.value === "") {
-    alerta.value = "El campo teléfono no puede estar vacio"
-    ocultarAlerta()
-  }
-  else if (telefono.value.length < 10) {
-    alerta.value = "El campo teléfono debe tener al menos 10 números"
-    ocultarAlerta()
-  }
-  else if (fecha_nacimiento.value === "") {
-    alerta.value = "El campo fecha de nacimiento no puede estar vacio"
-    ocultarAlerta()
-  }
-  else {
-    const fecha = new Date();
-    const fechaActual = fecha.toISOString().split('T')[0];
-    let edad = fechaActual.split("-")[0] - fecha_nacimiento.value.split("-")[0];
-    if (fechaActual.split("-")[1] < fecha_nacimiento.value.split("-")[1]) {
-      edad--;
-    }
-    if (edad < 18) {
-      alerta.value = "Debe ser mayor de 18 años para registrarse";
-      ocultarAlerta()
-    } else {
-      alerta.value = "Formulario enviado correctamente"
-      ocultarAlerta()
-      registros.value.push({
-        nombre: nombre.value,
-        apellidos: apellidos.value,
-        tipo_documento: tipo_documento.value,
-        numero_documento: numero_documento.value,
-        genero: genero.value,
-        correo: correo.value,
-        telefono: telefono.value,
-        fecha_nacimiento: fecha_nacimiento.value,
-        edad:edad1.value
-      })
-      console.log(registros.value)
-    }
-  }
-  } else {
-registros.value [index].nombre= nombre.value
-registros.value [index].apellidos= apellidos.value
-bd=true
-  }
-  
-} 
-
-
-
-
-
-/* let contador=ref(0)
-function contar(){
-  contador.value+=1
-} */
-
-
-/* let num =ref(0)
-let num2 =ref(0)
-let telefono = ref("312446579")
-telefono.value="345"
-let nombre = ref("Contreras")
-nombre.value = telefono.value */
-
-</script>
-
-<style scoped>
-h3 {
-  color: red;
-}
-
-#padre {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  width: 100%;
-  padding: 10px;
-}
-
-th, tr, td{
-  border: 2px solid blue;
-}
-
-form {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  width: 80%;
-  margin-bottom: 20px;
-  background-color: #5a007d;
-  padding: 10px;
-}
-
-input{
-  width: 100%;
-}
-
-select {
-  width: 100%;
-  padding: 2px;
-} 
-</style> -->
